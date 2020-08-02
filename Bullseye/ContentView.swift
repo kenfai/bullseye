3eec825c8cf9667c073c93f5dede48e49ad35566
KenFai <ngkenfai@gmail.com>
2020-08-02 17:20:15 +0800

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Ginger on 02/08/2020.
+//  Copyright © 2020 Ginger. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
